e782bc83019d7e9e742a9c50d6cdd1f78c549ab6
carboni-rob <roberto.carboni@flexmr.org>
2019-09-08 17:21:49 +0100

    Info tests
    Signed-off-by: carboni-rob <roberto.carboni@flexmr.org>

--- src/Info/__tests__/Info.test.js
+++ src/Info/__tests__/Info.test.js
@@ -1,3 +1,48 @@
+/* eslint-disable @typescript-eslint/camelcase */
 import React from "react";
 import { render } from "@testing-library/react";
+import { Context } from "../../Store";
 import Info from "../Info";
+
+const store = {
+  userData: {
+    user: {
+      name: "Roberto",
+      company: "Black Swan Data",
+      email: "carboni@robertocarboni.it",
+      followers: 100,
+      following: 100,
+      avatar_url: "fake/avatar/path"
+    },
+    orgs: [
+      {
+        id: 1,
+        login: "test1",
+        url: "test1/orgs/url",
+        avatar_url: "test1/avatar/url"
+      }
+    ]
+  },
+  userRepos: [
+    {
+      id: 1,
+      name: "test1",
+      clone_url: "test1/repos/url"
+    }
+  ]
+};
+
+describe("<Info />", () => {
+  const dispatch = jest.fn();
+  const tree = (
+    <Context.Provider value={{ store, dispatch }}>
+      <Info />
+    </Context.Provider>
+  );
+
+  it("should match last snapshot", () => {
+    const { container } = render(tree);
+
+    expect(container.firstChild).toMatchSnapshot();
+  });
+});
