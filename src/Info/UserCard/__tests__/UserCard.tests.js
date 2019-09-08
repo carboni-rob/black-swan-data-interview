15f53bc74270b34e6af5755d634a9e666f2da75b
carboni-rob <roberto.carboni@flexmr.org>
2019-09-08 17:06:36 +0100

    Completed UserCard tests
    Signed-off-by: carboni-rob <roberto.carboni@flexmr.org>

--- src/Info/UserCard/__tests__/UserCard.tests.js
+++ src/Info/UserCard/__tests__/UserCard.tests.js
@@ -7,7 +7,7 @@ describe("<UserCard />", () => {
   const user = {
     name: "Roberto",
     company: "Black Swan Data",
-    email: "carboni@robertocarboi.it",
+    email: "carboni@robertocarboni.it",
     followers: 100,
     following: 100,
     avatar_url: "fake/avatar/path"
@@ -33,5 +33,53 @@ describe("<UserCard />", () => {
 
     expect(avatar).toHaveProperty("className", "userAvatar");
     expect(avatar).toHaveProperty("src", "http://localhost/fake/avatar/path");
+  });
+
+  it("should contain a user name field with the appropriate text", () => {
+    const { getByText } = render(tree);
+
+    getByText("User name: Roberto");
+  });
+
+  it("should contain a user name field with 'not set' text when username is missing", () => {
+    const { getByText } = render(<UserCard {...user} name="" />);
+
+    getByText("User name: not set");
+  });
+
+  it("should contain a company field with the appropriate text", () => {
+    const { getByText } = render(tree);
+
+    getByText("Company: Black Swan Data");
+  });
+
+  it("should contain a company field with 'not set' text when company name is missing", () => {
+    const { getByText } = render(<UserCard {...user} company="" />);
+
+    getByText("Company: not set");
+  });
+
+  it("should contain an email field with the appropriate text", () => {
+    const { getByText } = render(tree);
+
+    getByText("Email: carboni@robertocarboni.it");
+  });
+
+  it("should contain an email field with 'not set' text when email is missing", () => {
+    const { getByText } = render(<UserCard {...user} email="" />);
+
+    getByText("Email: not set");
+  });
+
+  it("should contain a followers field with the appropriate followers count", () => {
+    const { getByText } = render(tree);
+
+    getByText("Followers: 100");
+  });
+
+  it("should contain a following field with the appropriate following count", () => {
+    const { getByText } = render(tree);
+
+    getByText("Following: 100");
   });
 });
