061ef0def78dd0007d9bc1b05d40ede85e7f2bbd
carboni-rob <roberto.carboni@flexmr.org>
2019-09-08 16:12:02 +0100

    Added ReposCard test file
    Signed-off-by: carboni-rob <roberto.carboni@flexmr.org>

--- src/Info/ReposCard/__tests__/ReposCard.js
+++ src/Info/ReposCard/__tests__/ReposCard.js
@@ -4,8 +4,28 @@ import { render } from "@testing-library/react";
 import ReposCard from "../ReposCard";
 
 describe("<ReposCard />", () => {
-  const tree = <ReposCard />;
+  const repos = [
+    {
+      id: 1,
+      name: "test1",
+      clone_url: "test1/repos/url"
+    },
+    {
+      id: 2,
+      name: "test2",
+      clone_url: "test2/repos/url"
+    },
+    {
+      id: 3,
+      name: "test3",
+      clone_url: "test3/repos/url"
+    }
+  ];
+
+  const tree = <ReposCard repos={repos} />;
   it("should match last snapshot", () => {
     const { container } = render(tree);
+
+    expect(container.firstChild).toMatchSnapshot();
   });
 });
