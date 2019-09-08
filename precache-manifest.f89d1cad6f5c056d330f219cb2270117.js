8d696ab86d245af83c1b82b5ea57f0096bb31a16
carboni-rob <roberto.carboni@flexmr.org>
2019-09-08 18:58:56 +0100

    Updates

---
+++ precache-manifest.f89d1cad6f5c056d330f219cb2270117.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5e83bd33b30c737f4f56d605dec56ae5",
+    "url": "/carboni-rob/black-swan-data-interview/index.html"
+  },
+  {
+    "revision": "2a362f42f495ad5d2b94",
+    "url": "/carboni-rob/black-swan-data-interview/static/css/2.8c467cd4.chunk.css"
+  },
+  {
+    "revision": "cc81097111d4c5f86d61",
+    "url": "/carboni-rob/black-swan-data-interview/static/css/main.30e1b4a0.chunk.css"
+  },
+  {
+    "revision": "2a362f42f495ad5d2b94",
+    "url": "/carboni-rob/black-swan-data-interview/static/js/2.191d0bb5.chunk.js"
+  },
+  {
+    "revision": "cc81097111d4c5f86d61",
+    "url": "/carboni-rob/black-swan-data-interview/static/js/main.02c9fcd3.chunk.js"
+  },
+  {
+    "revision": "a58d582741e64dfec1e7",
+    "url": "/carboni-rob/black-swan-data-interview/static/js/runtime~main.6508dba6.js"
+  },
+  {
+    "revision": "0ba2aa20e2c2ce80e9a2db5b07198464",
+    "url": "/carboni-rob/black-swan-data-interview/static/media/Octocat.0ba2aa20.png"
+  },
+  {
+    "revision": "21233f2601d4bed69fd43dbb92d8b552",
+    "url": "/carboni-rob/black-swan-data-interview/static/media/black_swan_logo.21233f26.png"
+  }
+]);
