40dfb8c544293d3950f8b8b98cb9e766d517889f
carboni-rob <roberto.carboni@flexmr.org>
2019-09-08 15:18:17 +0100

    Added OrgsCard test file
    Signed-off-by: carboni-rob <roberto.carboni@flexmr.org>

--- src/Info/OrgsCard/__tests__/OrgsCard.test.js
+++ src/Info/OrgsCard/__tests__/OrgsCard.test.js
@@ -1,1 +1,35 @@
+/* eslint-disable @typescript-eslint/camelcase */
 import React from "react";
+import { render } from "@testing-library/react";
+import OrgsCard from "../OrgsCard";
+
+describe("<OrgsCard />", () => {
+  const orgs = [
+    {
+      id: 1,
+      login: "test1",
+      url: "test1/orgs/url",
+      avatar_url: "test1/avatar/url"
+    },
+    {
+      id: 2,
+      login: "test2",
+      url: "test2/orgs/url",
+      avatar_url: "test2/avatar/url"
+    },
+    {
+      id: 3,
+      login: "test3",
+      url: "test3/orgs/url",
+      avatar_url: "test3/avatar/url"
+    }
+  ];
+
+  const tree = <OrgsCard orgs={orgs} />;
+
+  it("should match last snapshot", () => {
+    const { container } = render(tree);
+
+    expect(container.firstChild).toMatchSnapshot();
+  });
+});
