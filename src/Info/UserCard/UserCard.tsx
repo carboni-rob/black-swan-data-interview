e0ac09648a7df07d95ea44abe793120ab2d74b20
carboni-rob <roberto.carboni@flexmr.org>
2019-09-08 16:20:43 +0100

    Added types to UserCard
    Signed-off-by: carboni-rob <roberto.carboni@flexmr.org>

--- src/Info/UserCard/UserCard.tsx
+++ src/Info/UserCard/UserCard.tsx
@@ -1,19 +1,32 @@
+/* eslint-disable @typescript-eslint/camelcase */
 import React from "react";
 
 type Props = {
-  user: any;
+  name: string;
+  company: string;
+  email: string;
+  followers: number;
+  following: number;
+  avatar_url: string;
 };
 
-const UserCard = ({ user }: Props): JSX.Element => {
+const UserCard = ({
+  name,
+  company,
+  email,
+  followers,
+  following,
+  avatar_url
+}: Props): JSX.Element => {
   return (
     <div className="card">
       <h2>User Info:</h2>
-      <img src={user.avatar_url} alt="User Avatar" className="userAvatar" />
-      <h3>User name: {user.name || "not set"}</h3>
-      <h3>Company: {user.company || "not set"}</h3>
-      <h3>Email: {user.email || "not set"}</h3>
-      <h3>Followers: {user.followers}</h3>
-      <h3>Following: {user.following}</h3>
+      <img src={avatar_url} alt="User Avatar" className="userAvatar" />
+      <h3>User name: {name || "not set"}</h3>
+      <h3>Company: {company || "not set"}</h3>
+      <h3>Email: {email || "not set"}</h3>
+      <h3>Followers: {followers}</h3>
+      <h3>Following: {following}</h3>
     </div>
   );
 };
